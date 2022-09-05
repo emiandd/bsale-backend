e7bdd0616c7ba2d3f87209469e22b11a6e9e7f4c
emiandd <emilandresaravena@icloud.com>
2022-09-04 23:25:24 -0300

    server pointing to deployed frontend

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin','http://localhost'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin','https://bsale-store.netlify.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET');
